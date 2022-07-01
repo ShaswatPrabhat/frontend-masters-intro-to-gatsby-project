262477a18c72e4fe3d7584ddd5f51552b62adfcb
Shaswat Prabhat <spd1238@gmail.com>
2022-07-01 12:04:24 +0530

    Add MDX integration and workflow, basic styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,8 @@
 import { graphql, Link, useStaticQuery } from 'gatsby';
 import Seo from './seo';
 import React from 'react';
+import '../styles/global.css';
+import { header, content } from '../styles/layout.module.css';
 
 const Layout = ({ children, title, description, image, path }) => {
   const res = useStaticQuery(graphql`
@@ -17,13 +19,13 @@ const Layout = ({ children, title, description, image, path }) => {
   return (
     <>
       <Seo title={title} description={description} image={image} path={path} />
-      <header>
+      <header className={header}>
         <Link to="/">{meta.title}</Link>
         <nav>
           <Link to="/about">About</Link>
         </nav>
       </header>
-      <main>{children}</main>
+      <main className={content}>{children}</main>
     </>
   );
 };
